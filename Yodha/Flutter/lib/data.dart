78cfbb81265c30a96ee047a6b2ab65f0deb615a4
Ayush783 <ayushc45xxx@gmail.com>
2020-10-04 02:42:28 +0530

    completed stories data with dummy data

---
+++ Yodha/Flutter/lib/data.dart
@@ -0,0 +1,72 @@
+List<Map<String, String>> stories = [
+  {
+    "stories":
+        "I can feel the edge of the grief washing up on my toes. It lingers there, then recedes like the tide. I'm gonna let it come, and I'm going to LET IT GO.",
+    "name": "Taddie Diagnosed at 38",
+    "link":
+        "https://www.youngsurvival.org/blog/survivor-stories/survivor/taddie-diagnosed-at-38",
+  },
+  {
+    "stories":
+        "Covid has made this already hard situation much harder as I had to do my last few chemo treatments alone, surgery alone, radiation alone.",
+    "name": "Sarah Diagnosed at 41",
+    "link":
+        "https://www.youngsurvival.org/blog/survivor-stories/survivor/sarah-diagnosed-at-41",
+  },
+  {
+    "stories":
+        "As of September 2020 I have been cancer free since March 2020. I have hormone therapy left for five years. It's okay. I will do it. I will make it. You will make it too.",
+    "name": "Sabreen Diagnosed at 30",
+    "link":
+        "https://www.youngsurvival.org/blog/survivor-stories/survivor/sabreen-diagnosed-at-30",
+  },
+  {
+    "stories":
+        "I was diagnosed at the age of 19 and I am now 23 years old and still battling cancer.",
+    "name": "JaLaine Diagnosed at 19",
+    "link":
+        "https://www.youngsurvival.org/blog/survivor-stories/survivor/jalaine-diagnosed-at-19",
+  },
+  {
+    "stories":
+        "All I can tell you is this—I am 42 years old today and have carried scars from my cancer inside and outside when I barely just turned 31 years old.",
+    "name": "Uyen Diagnosed at 31",
+    "link":
+        "https://www.youngsurvival.org/blog/survivor-stories/survivor/uyen-diagnosed-at-31",
+  },
+  {
+    "stories":
+        "Pathology results showed a complete response but I do live in fear sometimes. In the meantime, I have not given up the notion of having another baby.",
+    "name": "Deirdre Diagnosed at 38",
+    "link":
+        "https://www.youngsurvival.org/blog/survivor-stories/survivor/deirdre-diagnosed-at-38",
+  },
+  {
+    "stories":
+        "Throughout my testing I was constantly reassured by multiple doctors and nurses, 'You are so young, this is probably just benign, you will be just fine.",
+    "name": "Elizabeth Diagnosed at 25",
+    "link":
+        "https://www.youngsurvival.org/blog/survivor-stories/survivor/elizabeth-diagnosed-at-25",
+  },
+  {
+    "stories":
+        "I was 27 and a wife, mother of a 3-year-old son and 1-year-old daughter, and a full-time nursing student. With all of that going on, I didn't have time to deal with the lump I felt in my right breast.",
+    "name": "Klara Diagnosed at 28",
+    "link":
+        "https://www.youngsurvival.org/blog/survivor-stories/survivor/klara-diagnosed-at-28",
+  },
+  {
+    "stories":
+        "Most women I have met in support groups are much older than I am, so it's hard for them to completely understand how tough it can be to get this diagnosis at a young age!",
+    "name": "Andrea Diagnosed at 36",
+    "link":
+        "https://www.youngsurvival.org/blog/survivor-stories/survivor/andrea-diagnosed-at-36",
+  },
+  {
+    "stories":
+        "The nurses told me that I have some of the best angels that helped me find this cancer so early. Most women don't detect IBC until stage 4.",
+    "name": "Jennifer Diagnosed at 31",
+    "link":
+        "https://www.youngsurvival.org/blog/survivor-stories/survivor/jennifer-diagnosed-at-31",
+  },
+];
--- Yodha/Flutter/lib/screens/cancer_patient_screens/stories_screen.dart
+++ Yodha/Flutter/lib/screens/cancer_patient_screens/stories_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:ne_proj/const.dart';
+import 'package:ne_proj/data.dart';
+import 'package:ne_proj/widgets/spacing.dart';
 
 class StoriesScreen extends StatefulWidget {
   @override
@@ -8,6 +11,60 @@ class StoriesScreen extends StatefulWidget {
 class _StoriesScreenState extends State<StoriesScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(
+      itemCount: stories.length,
+      itemBuilder: (context, index) {
+        return Container(
+          height: 540,
+          width: 360,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('images/${index + 1}.jpg'),
+                fit: BoxFit.fill,
+                colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.2), BlendMode.dstATop)),
+          ),
+          child: Padding(
+            padding: EdgeInsets.only(left: 20.0, right: 20, top: 95),
+            child: Column(
+              children: [
+                Wrap(
+                  children: [
+                    Text(
+                      stories[index]["stories"],
+                      style: title.copyWith(fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+                WidgetSpacing(
+                  top: 60,
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Wrap(
+                    children: [
+                      Text(
+                        "-${stories[index]["name"]}",
+                        style: title.copyWith(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                        textAlign: TextAlign.right,
+                      ),
+                    ],
+                  ),
+                ),
+                WidgetSpacing(
+                  top: 40,
+                ),
+                Text(
+                  'Read full story here',
+                  style: title.copyWith(fontSize: 18),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 }
